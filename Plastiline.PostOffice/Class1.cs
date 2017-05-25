fd2a46090e5ce1ac487e1d979ff9562a809b2a61
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2017-05-25 14:17:55 +0200

    Setting up dependencies in nuspec files

--- Plastiline.PostOffice/Class1.cs
+++ Plastiline.PostOffice/Class1.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Plastiline.PostOfficeApi;
+using System;
 
 namespace Plastiline.PostOffice
 {
     public class Class1
     {
+        public void Foo()
+        {
+            var command = new SendLetterCommand();
+        }
     }
 }
--- Plastiline.PostOfficeApi/SendLetterCommand.cs
+++ Plastiline.PostOfficeApi/SendLetterCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Plastiline.PostOfficeApi
 {
-    public class Class1
+    public class SendLetterCommand
     {
     }
 }
